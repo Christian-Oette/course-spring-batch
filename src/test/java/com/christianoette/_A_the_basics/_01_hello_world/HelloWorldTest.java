13a009138e56b4b07bfa67ff037916495bcb11bd
chris <christian.oette@web.de>
2020-04-16 14:11:53 +0200

    Prepare Hello World example
    Update code for first video

--- src/test/java/com/christianoette/_A_the_basics/_01_hello_world/HelloWorldTest.java
+++ src/test/java/com/christianoette/_A_the_basics/_01_hello_world/HelloWorldTest.java
@@ -2,56 +2,13 @@ package com.christianoette._A_the_basics._01_hello_world;
 
 
 import org.junit.jupiter.api.Test;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.step.tasklet.TaskletStep;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.batch.test.JobLauncherTestUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@SpringBootTest(classes = HelloWorldTest.TestConfig.class)
+@SpringBootTest
 class HelloWorldTest {
 
-    @Autowired
-    private JobLauncherTestUtils jobLauncherTestUtils;
-
     @Test
-    void runJob() throws Exception {
-        jobLauncherTestUtils.launchJob();
+    void test() {
+        // start here!
     }
-
-    @Configuration
-    @EnableBatchProcessing
-    static class TestConfig {
-
-        @Autowired
-        private JobBuilderFactory jobBuilderFactory;
-
-        @Autowired
-        private StepBuilderFactory stepBuilderFactory;
-
-        @Bean
-        public Job job() {
-            TaskletStep step = stepBuilderFactory.get("myFirstStep")
-                    .tasklet((stepContribution, chunkContext) -> {
-                        System.out.println("Hello world");
-                        return RepeatStatus.FINISHED;
-                    }).build();
-
-            return jobBuilderFactory.get("myJob")
-                    .start(step)
-                    .build();
-        }
-
-        @Bean
-        public JobLauncherTestUtils utils() {
-            return new JobLauncherTestUtils();
-        }
-    }
-
 }
