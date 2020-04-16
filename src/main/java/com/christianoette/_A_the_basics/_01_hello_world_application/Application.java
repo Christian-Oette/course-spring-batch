c4c7a6fcaf371bad4f661719dcf6274b77019a42
chris <christian.oette@web.de>
2020-04-16 18:56:08 +0200

    First app fully implemented

--- src/main/java/com/christianoette/_A_the_basics/_01_hello_world_application/Application.java
+++ src/main/java/com/christianoette/_A_the_basics/_01_hello_world_application/Application.java
@@ -1,7 +1,11 @@
 package com.christianoette._A_the_basics._01_hello_world_application;
 
 import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -16,8 +20,12 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("/context/without-web-context.properties")
 public class Application {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext appContext = SpringApplication.run(Application.class, args);
+	public static void main(String[] args) throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, InterruptedException {
+		ConfigurableApplicationContext appContext =
+				SpringApplication.run(Application.class, args);
+
+		TriggerJobService triggerJobService = appContext.getBean(TriggerJobService.class);
+		triggerJobService.runJob();
 	}
 
 }
---
+++ src/main/java/com/christianoette/_A_the_basics/_01_hello_world_application/TriggerJobService.java
@@ -0,0 +1,39 @@
+package com.christianoette._A_the_basics._01_hello_world_application;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TriggerJobService {
+
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    public TriggerJobService(JobLauncher jobLauncher, Job job) {
+        this.jobLauncher = jobLauncher;
+        this.job = job;
+    }
+
+    public void runJob() throws JobParametersInvalidException,
+            JobExecutionAlreadyRunningException,
+            JobRestartException,
+            JobInstanceAlreadyCompleteException, InterruptedException {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addParameter("outputText", new JobParameter("My first spring boot app"))
+                .toJobParameters();
+
+        jobLauncher.run(job, jobParameters);
+
+        Thread.sleep(3000);
+
+        JobParameters jobParameters2 = new JobParametersBuilder()
+                .addParameter("outputText", new JobParameter("Second run"))
+                .toJobParameters();
+
+        jobLauncher.run(job, jobParameters2);
+    }
+}
