c3926bcabe6a91319e039e9cd611d26f4e2508e4
chris <christian.oette@web.de>
2020-04-16 15:53:10 +0200

    Add first test implementation to full-sources branch

--- src/test/java/com/christianoette/_A_the_basics/_01_hello_world/HelloWorldTest.java
+++ src/test/java/com/christianoette/_A_the_basics/_01_hello_world/HelloWorldTest.java
@@ -2,13 +2,66 @@ package com.christianoette._A_the_basics._01_hello_world;
 
 
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootTest
+import java.time.Clock;
+import java.time.LocalTime;
+import java.util.Map;
+
+@SpringBootTest(classes = HelloWorldTest.TestConfig.class)
 class HelloWorldTest {
 
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
     @Test
-    void test() {
-        // start here!
+    void test() throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addParameter("outputText", new JobParameter("Hello Spring Batch"))
+                .toJobParameters();
+        jobLauncherTestUtils.launchJob(jobParameters);
+    }
+
+    @Configuration
+    @EnableBatchProcessing
+    static class TestConfig {
+
+        @Autowired
+        private JobBuilderFactory jobBuilderFactory;
+
+        @Autowired
+        private StepBuilderFactory stepBuilderFactory;
+
+        @Bean
+        public Job helloWorldJob() {
+            Step step = stepBuilderFactory.get("step")
+                    .tasklet((contribution, chunkContext) -> {
+                        Map<String, Object> jobParameters = chunkContext.getStepContext()
+                                .getJobParameters();
+                        Object outputText = jobParameters.get("outputText");
+                        System.out.println(outputText);
+                        return RepeatStatus.FINISHED;
+                    }).build();
+
+            return jobBuilderFactory.get("helloWorldJob")
+                    .start(step)
+                    .build();
+        }
+
+        @Bean
+        public JobLauncherTestUtils utils() {
+            return new JobLauncherTestUtils();
+        }
     }
 }
