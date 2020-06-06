b28ddc8fe1ad4307f416ca0a745ea3f999d2e13b
chris <christian.oette@web.de>
2020-06-06 21:32:44 +0200

    Persistent Data model application prepared

--- src/main/java/com/christianoette/_B_the_data_model/_01_persistent_repository/Application.java
+++ src/main/java/com/christianoette/_B_the_data_model/_01_persistent_repository/Application.java
@@ -1,7 +1,11 @@
 package com.christianoette._B_the_data_model._01_persistent_repository;
 
 import com.christianoette.utils.components.trigger.CourseUtilsDefaultTrigger;
-import org.springframework.batch.core.JobExecutionException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -14,12 +18,21 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan(basePackageClasses = {Application.class, CourseUtilsDefaultTrigger.class})
 @EnableBatchProcessing
 @EntityScan(basePackageClasses = Application.class)
-@PropertySource("/context/without-web-context.properties")
+@PropertySource("/context/persistent-h2-context.properties")
 public class Application {
 
-	public static void main(String[] args) throws JobExecutionException, InterruptedException {
+	private static final Logger LOGGER = LogManager.getLogger(Application.class);
+
+	public static void main(String[] args) {
 		ConfigurableApplicationContext appContext = SpringApplication.run(Application.class, args);
 		CourseUtilsDefaultTrigger trigger = appContext.getBean(CourseUtilsDefaultTrigger.class);
-		trigger.runJobs();
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addParameter("outputText", new JobParameter("Hello persistent spring batch job"))
+				.toJobParameters();
+		try {
+			trigger.runJobs(jobParameters);
+		} catch (Exception ex) {
+			LOGGER.error(ex.getMessage());
+		}
 	}
 }
