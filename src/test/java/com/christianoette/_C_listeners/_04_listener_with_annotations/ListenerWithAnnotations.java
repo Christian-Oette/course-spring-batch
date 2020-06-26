716989d94ef6efc4e60120a3bd8793ba11c390b1
chris <christian.oette@web.de>
2020-06-26 20:10:46 +0200

    ListenerWithAnnotations prepared

--- src/test/java/com/christianoette/_C_listeners/_04_listener_with_annotations/ListenerWithAnnotations.java
+++ src/test/java/com/christianoette/_C_listeners/_04_listener_with_annotations/ListenerWithAnnotations.java
@@ -1,20 +1,13 @@
 package com.christianoette._C_listeners._04_listener_with_annotations;
 
-import org.springframework.batch.core.annotation.AfterChunk;
-import org.springframework.batch.core.annotation.BeforeChunk;
-import org.springframework.batch.core.scope.context.ChunkContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ListenerWithAnnotations {
 
-    @BeforeChunk
-    public void before(ChunkContext chunkContext) {
-        System.out.println("before chunk");
-    }
+    private static final Logger LOGGER = LogManager.getLogger(ListenerWithAnnotations.class);
 
-    @AfterChunk
-    public void after(ChunkContext chunkContext) {
-        System.out.println("after chunk");
-    }
+
 }
