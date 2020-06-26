47e251c119bc5d64c5f751167694de2e90e8de3f
chris <christian.oette@web.de>
2020-06-26 14:15:37 +0200

    JobExecutionerListenerAsComponentTest prepared

--- src/test/java/com/christianoette/_C_listeners/_02_job_execution_listener_component/JobListenerAsComponent.java
+++ src/test/java/com/christianoette/_C_listeners/_02_job_execution_listener_component/JobListenerAsComponent.java
@@ -4,11 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
-import org.springframework.batch.core.configuration.annotation.JobScope;
-import org.springframework.stereotype.Component;
 
-@Component
-@JobScope
 public class JobListenerAsComponent implements JobExecutionListener {
 
     private static final Logger LOGGER = LogManager.getLogger(JobListenerAsComponent.class);
